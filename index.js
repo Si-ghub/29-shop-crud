98b15d9a435a94da263554c1a8921d0680d0bc83
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-07-18 16:13:44 +0300

    Shop completed

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ onlineKioskas.order('Barbora');
 */
 
 onlineKioskas.orderPrice('Augustas');
-// Augustas order: 5.30 EUR.
+// Augustas order: 4.40 EUR.
 
 onlineKioskas.orderPrice('Barbora');
 // Barbora order: 5.50 EUR.
@@ -91,7 +91,7 @@ onlineKioskas.pay('Augustas', 400);
 // Need more money!
 
 onlineKioskas.pay('Augustas', 600);
-// Here is your 0.70 EUR change!\nThank you for purchasing at "Meskiuko kioskas"!
+// Here is your 1.60 EUR change!\nThank you for purchasing at "Meskiuko kioskas"!
 
 onlineKioskas.pay('Barbora', 550);
 // Thank you for purchasing at "Meskiuko kioskas"!
@@ -99,17 +99,20 @@ onlineKioskas.pay('Barbora', 550);
 onlineKioskas.addItemToCart('Augustas', 3, 1);
 // You can not add items to already paid cart!
 
-//onlineKioskas.addItemToCart('John', 3, 1);
-//onlineKioskas.addItemToCart('John', 4, 2);
-//onlineKioskas.addItemToCart('John', 5, 1);
+onlineKioskas.createCart('John');
+// John have an open cart at "Meskiuko kioskas"!
 
-//onlineKioskas.addItemToCart('John', 1, 1);
+onlineKioskas.addItemToCart('John', 3, 1);
+onlineKioskas.addItemToCart('John', 4, 2);
+onlineKioskas.addItemToCart('John', 5, 1);
+
+onlineKioskas.addItemToCart('John', 1, 1);
 // Item is out of stock!
 
-//onlineKioskas.order('John');
+onlineKioskas.order('John');
 /*
 {
-    owner: 'Augustas',
+    owner: 'John',
     items: [
         { id: 3, count: 1 },
         { id: 4, count: 2 },
@@ -117,11 +120,12 @@ onlineKioskas.addItemToCart('Augustas', 3, 1);
     ]
 }
 */
+onlineKioskas.items();
 
-//onlineKioskas.orderPrice('John');
-// Augustas order: 5.60 EUR.
+onlineKioskas.orderPrice('John');
+// Augustas order: 4.70 EUR.
 
-//onlineKioskas.shopSummary();
+onlineKioskas.shopSummary();
 // Summary for the "Meskiuko kioskas"
 // ====================
 // Items sold: 12
